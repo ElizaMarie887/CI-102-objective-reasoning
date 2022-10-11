fb65baa4c96557b798f004ef2cd6e0e0b69a1ec4
Denise "Eliza" Cherry-Russell <108440338+ElizaMarie887@users.noreply.github.com>
2022-10-11 14:17:05 -0500

    Assignment Complete

--- main.js
+++ main.js
@@ -1,6 +1,90 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
+/*
+isAdmin
+parameters: user object
+
+user ={
+    firstName: String
+    lastName: String
+    userRole: String
+}
+
+return boolean - true or false
+
+
+*/
+
+function isAdmin(user){
+    if(user.userRole === 'ADMIN'){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+function getEmail(user){
+   let userEmail = (`${user.firstName}.${user.lastName}@codeimmersives.com`);
+   return userEmail.toLowerCase();
+}
+
+
+
+function getPlaylistLength(playlist){
+    return playlist.songs.length 
+}
+/*
+getHardestHomework
+parameters - homework array
+
+homework = {
+    name: string
+    average score: number
+}
+
+return type: string
+
+*/
+
+function getHardestHomework(homeworkArray){
+    if(homeworkArray.length === 0){
+        return ''
+    }
+
+    let lowestHw = homeworkArray[0]
+
+    for(let i = 0; i < homeworkArray.length; i++){
+        if( homeworkArray[i].averageScore < lowestHw.averageScore){
+            lowestHw = homeworkArray[i];
+        }
+    }
+    
+return lowestHw.name;
+    
+}
+/*
+createPhonebook
+parameters: 
+namesArray - string
+numbersArray - string
+
+return phonebook object{
+
+    'name' : 'number'   string : string
+}
+*/
+
+function createPhonebook(namesArray, numbersArray){
+    let phonebook = {}
+
+    for(let i = 0; i < namesArray.length ;i++){
+        phonebook[namesArray[i]] = numbersArray[i];
+    }
+return phonebook
+}
 
 
 
